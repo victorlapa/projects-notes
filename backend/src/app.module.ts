1a18126e25dbc97e358c811cb8d4fe719820709f
Victor Lapa <victor.lapa@catolicasc.edu.br>
2025-09-01 02:08:10 -0300

    fix bootstrap error

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -13,7 +13,7 @@ import { SeedService } from './seed.service';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DATABASE_HOST || 'localhost',
-      port: parseInt(process.env.DATABASE_PORT) || 5432,
+      port: parseInt(process.env.DATABASE_PORT || '5432'),
       username: process.env.DATABASE_USER || 'postgres',
       password: process.env.DATABASE_PASSWORD || 'postgres',
       database: process.env.DATABASE_NAME || 'billor',
